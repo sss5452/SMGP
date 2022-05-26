85eed8836066da0076ef272b1f9ea5b83c94c9d4
sss5452 <71081825+sss5452@users.noreply.github.com>
2022-05-26 21:18:49 +0900

    .
    .

---
+++ CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Enemy.java
@@ -0,0 +1,136 @@
+package net.scgyong.and.cookierun.game;
+
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+import net.scgyong.and.cookierun.R;
+import net.scgyong.and.cookierun.framework.interfaces.BoxCollidable;
+import net.scgyong.and.cookierun.framework.interfaces.GameObject;
+import net.scgyong.and.cookierun.framework.objects.SheetSprite;
+import net.scgyong.and.cookierun.framework.res.Metrics;
+
+import java.util.ArrayList;
+
+public class Enemy extends SheetSprite implements BoxCollidable {
+
+    private static final float FRAMES_PER_SECOND = 8f;
+    private static final String TAG = Enemy.class.getSimpleName();
+
+    static {
+        State.initRects();
+    }
+
+    private enum State {
+        run, jump, doubleJump, falling, slide, COUNT;
+        Rect[] srcRects() {
+            return rectsArray[this.ordinal()];
+        }
+        void applyInsets(RectF dstRect) {
+            float[] inset = insets[this.ordinal()];
+            float w = dstRect.width();
+            float h = dstRect.height();
+            dstRect.left += w * inset[0];
+            dstRect.top += h * inset[1];
+            dstRect.right -= w * inset[2];
+            dstRect.bottom -= h * inset[3];
+        }
+        static Rect[][] rectsArray;
+        static void initRects() {
+            int[][] indices = {
+                    new int[] { 0,1,2,3,4,5,6,7,8,9,10}, // run
+                    new int[] { 0,}, // jump
+                    new int[] { 1, 2, 3, 4 }, // doubleJump
+                    new int[] { 0 }, // falling
+                    new int[] { 9, 10 },    //slide
+            };
+            rectsArray = new Rect[indices.length][];
+            for (int r = 0; r < indices.length; r++) {
+                int[] ints = indices[r];
+                Rect[] rects = new Rect[ints.length];
+
+                for (int i = 0; i < ints.length; i++) {
+                    int idx = ints[i];
+                    int l = (idx % 100) * 32;
+                    Rect rect = new Rect(l, 0, l+32, 32);
+                    rects[i] = rect;
+                }
+
+                rectsArray[r] = rects;
+            }
+        }
+        float[][] insets = {
+                new float[] { 0.10f, 0.05f, 0.10f, 0.00f }, // run
+                new float[] { 0.10f, 0.20f, 0.10f, 0.00f }, // jump
+                new float[] { 0.10f, 0.15f, 0.10f, 0.00f }, // doubleJump
+                new float[] { 0.10f, 0.05f, 0.10f, 0.00f }, // falling
+                new float[] { 0.00f, 0.50f, 0.00f, 0.00f }, // slide
+        };
+    }
+    private State state = State.run;
+    private final float jumpPower;
+    private final float gravity;
+    private float jumpSpeed;
+    protected RectF collisionBox = new RectF();
+    private float moveSpeed;
+    private float fliction;
+
+
+    public Enemy(float x, float y, float w, float h, int type) {
+        super(R.mipmap.frog_idle, FRAMES_PER_SECOND * 2);
+        this.x = x;
+        this.y = y;
+        jumpPower = Metrics.size(R.dimen.player_jump_power);
+        gravity = Metrics.size(R.dimen.player_gravity);
+        setDstRect(w, h);
+        setState(State.run);
+    }
+
+    @Override
+    public RectF getBoundingRect() {
+        return collisionBox;
+    }
+
+    @Override
+    public void update(float frameTime) {
+
+    }
+    public void findPlayer()
+    {
+
+    }
+
+    public void move(boolean isPushMove)
+    {
+
+        if ( isPushMove) {
+            moveSpeed = 10;
+            this.x += 0.1;
+            dstRect.offset(this.x, 0);
+            collisionBox.offset(this.x, 0);
+            return;
+        }
+        else{
+            return;
+        }
+
+    }
+
+    private void setState(State state) {
+        this.state = state;
+        if (state== State.jump)
+        {
+            ChangeBitmap(R.mipmap.frog_jump);
+        }
+        if (state== State.run)
+        {
+            ChangeBitmap(R.mipmap.frog_move);
+        }
+        if (state== State.jump)
+        {
+            ChangeBitmap(R.mipmap.frog_jump);
+        }
+        srcRects = state.srcRects();
+        collisionBox.set(dstRect);
+        state.applyInsets(collisionBox);
+    }
+}
---
+++ CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Player.java
@@ -0,0 +1,225 @@
+package net.scgyong.and.cookierun.game;
+
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+import net.scgyong.and.cookierun.R;
+import net.scgyong.and.cookierun.framework.interfaces.BoxCollidable;
+import net.scgyong.and.cookierun.framework.interfaces.GameObject;
+import net.scgyong.and.cookierun.framework.objects.SheetSprite;
+import net.scgyong.and.cookierun.framework.res.Metrics;
+
+import java.util.ArrayList;
+
+public class Player extends SheetSprite implements BoxCollidable {
+
+    private static final float FRAMES_PER_SECOND = 8f;
+    private static final String TAG = Player.class.getSimpleName();
+
+    static {
+        State.initRects();
+    }
+
+    private enum State {
+        run, jump, doubleJump, falling, slide, COUNT;
+        Rect[] srcRects() {
+            return rectsArray[this.ordinal()];
+        }
+        void applyInsets(RectF dstRect) {
+            float[] inset = insets[this.ordinal()];
+            float w = dstRect.width();
+            float h = dstRect.height();
+            dstRect.left += w * inset[0];
+            dstRect.top += h * inset[1];
+            dstRect.right -= w * inset[2];
+            dstRect.bottom -= h * inset[3];
+        }
+        static Rect[][] rectsArray;
+        static void initRects() {
+            int[][] indices = {
+                    new int[] { 0,1,2,3,4,5,6,7,8,9,10}, // run
+                    new int[] { 0,}, // jump
+                    new int[] { 1, 2, 3, 4 }, // doubleJump
+                    new int[] { 0 }, // falling
+                    new int[] { 9, 10 },    //slide
+            };
+            rectsArray = new Rect[indices.length][];
+            for (int r = 0; r < indices.length; r++) {
+                int[] ints = indices[r];
+                Rect[] rects = new Rect[ints.length];
+
+                for (int i = 0; i < ints.length; i++) {
+                    int idx = ints[i];
+                    int l = (idx % 100) * 32;
+                    Rect rect = new Rect(l, 0, l+32, 32);
+                    rects[i] = rect;
+                }
+
+                rectsArray[r] = rects;
+            }
+        }
+        float[][] insets = {
+                new float[] { 0.10f, 0.05f, 0.10f, 0.00f }, // run
+                new float[] { 0.10f, 0.20f, 0.10f, 0.00f }, // jump
+                new float[] { 0.10f, 0.15f, 0.10f, 0.00f }, // doubleJump
+                new float[] { 0.10f, 0.05f, 0.10f, 0.00f }, // falling
+                new float[] { 0.00f, 0.50f, 0.00f, 0.00f }, // slide
+        };
+    }
+    private State state = State.run;
+    private final float jumpPower;
+    private final float gravity;
+    private float jumpSpeed;
+    protected RectF collisionBox = new RectF();
+    private float moveSpeed;
+    private float fliction;
+
+
+    public Player(float x, float y, float w, float h) {
+        super(R.mipmap.frog_idle, FRAMES_PER_SECOND * 2);
+        this.x = x;
+        this.y = y;
+        jumpPower = Metrics.size(R.dimen.player_jump_power);
+        gravity = Metrics.size(R.dimen.player_gravity);
+        setDstRect(w, h);
+        setState(State.run);
+    }
+
+    @Override
+    public RectF getBoundingRect() {
+        return collisionBox;
+    }
+
+    @Override
+    public void update(float frameTime) {
+        float foot = collisionBox.bottom;
+        switch (state) {
+            case jump:
+            case doubleJump:
+            case falling:
+                float dy = jumpSpeed * frameTime;
+                jumpSpeed += gravity * frameTime;
+//            Log.d(TAG, "y=" + y + " dy=" + dy + " js=" + jumpSpeed);
+                if (jumpSpeed >= 0) {
+                    float platformTop = findNearestPlatformTop(foot);
+//                    Log.i(TAG, "foot="+foot + " ptop=" + platformTop);
+                    if (foot + dy >= platformTop) {
+                        dy = platformTop - foot;
+                        setState(State.run);
+                    }
+                }
+                y += dy;
+                dstRect.offset(0, dy);
+                collisionBox.offset(0, dy);
+                break;
+            case run:
+            case slide:
+                float platformTop = findNearestPlatformTop(foot);
+                if (foot < platformTop) {
+                    setState(State.falling);
+                    jumpSpeed = 0;
+                }
+                break;
+        }
+    }
+
+    private float findNearestPlatformTop(float foot) {
+        Platform platform = findNearestPlatform(foot);
+        if (platform == null) return Metrics.height;
+        return platform.getBoundingRect().top;
+    }
+
+    private Platform findNearestPlatform(float foot) {
+        Platform nearest = null;
+        MainGame game = MainGame.get();
+        ArrayList<GameObject> platforms = game.objectsAt(MainGame.Layer.platform.ordinal());
+        float top = Metrics.height;
+        for (GameObject obj: platforms) {
+            Platform platform = (Platform) obj;
+            RectF rect = platform.getBoundingRect();
+            if (rect.left > x || x > rect.right) {
+                continue;
+            }
+//            Log.d(TAG, "foot:" + foot + " platform: " + rect);
+            if (rect.top < foot) {
+                continue;
+            }
+            if (top > rect.top) {
+                top = rect.top;
+                nearest = platform;
+            }
+//            Log.d(TAG, "top=" + top + " gotcha:" + platform);
+        }
+        return nearest;
+    }
+    public void move(boolean isPushMove)
+    {
+
+        if ( isPushMove) {
+            moveSpeed = 10;
+            this.x += 0.1;
+            dstRect.offset(this.x, 0);
+            collisionBox.offset(this.x, 0);
+            return;
+        }
+        else{
+            return;
+        }
+
+    }
+
+    public void jump() {
+//        Log.d(TAG, "Jump");
+        if (state == State.run) {
+            setState(State.jump);
+            jumpSpeed = -jumpPower;
+        } else if (state == State.jump){
+            setState(State.doubleJump);
+            jumpSpeed = -jumpPower;
+        }
+    }
+
+
+
+    public void slide(boolean startsSlide) {
+        if (state == State.run && startsSlide) {
+            setState(State.slide);
+            return;
+        }
+        if (state == State.slide && !startsSlide) {
+            setState(State.run);
+            return;
+        }
+    }
+
+    public void fall() {
+        if (state != State.run) return;
+        float foot = collisionBox.bottom;
+        Platform platform = findNearestPlatform(foot);
+        if (platform == null) return;
+        if (!platform.canPass()) return;
+        setState(State.falling);
+        dstRect.offset(0, 0.001f);
+        collisionBox.offset(0, 0.001f);
+        jumpSpeed = 0;
+    }
+
+    private void setState(State state) {
+        this.state = state;
+        if (state== State.jump)
+        {
+            ChangeBitmap(R.mipmap.frog_jump);
+        }
+        if (state== State.run)
+        {
+            ChangeBitmap(R.mipmap.frog_move);
+        }
+        if (state== State.jump)
+        {
+            ChangeBitmap(R.mipmap.frog_jump);
+        }
+        srcRects = state.srcRects();
+        collisionBox.set(dstRect);
+        state.applyInsets(collisionBox);
+    }
+}
